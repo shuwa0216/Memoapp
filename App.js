af942fbbc268ad1dcde5723e4df32b7b285cf9b9
shuwa0216 <shuwa0216@yahoo.co.jp>
2019-07-13 10:03:11 +0900

    Signup,LoginScreenの作成

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import MemoEditScreen from './src/screens/MemoEditScreen';
+import SignupScreen from './src/screens/SignupScreen';
 import Appbar from './src/components/Appbar'
 
 export default class App extends React.Component {
@@ -9,7 +9,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoEditScreen />
+        <SignupScreen />
       </View>
     );
   }
